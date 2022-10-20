8df9ab1df6f35e91b84914f0438f8d2248ce3c71
HuanChong <15289350682@163.com>
2022-10-20 10:31:45 +0800

    [Chong.Huan] feat: mybatis @SelectProvider

--- src/main/java/com/huandc/model/Student.java
+++ src/main/java/com/huandc/model/Student.java
@@ -1,14 +1,12 @@
 package com.huandc.model;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @RequiredArgsConstructor
-@NoArgsConstructor
 @Setter
 @Getter
 @Entity(name = "students")
@@ -23,36 +21,4 @@ public class Student {
     Boolean sex;
 
     String tel;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Boolean getSex() {
-        return sex;
-    }
-
-    public void setSex(Boolean sex) {
-        this.sex = sex;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public void setTel(String tel) {
-        this.tel = tel;
-    }
 }
