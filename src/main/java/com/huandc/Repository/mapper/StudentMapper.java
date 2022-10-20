5a4ddcefdf57dd79394eef9889888b3fa69946bf
HuanChong <15289350682@163.com>
2022-10-20 10:38:49 +0800

    [Chong.Huan] feat: mybatis @SelectProvider without method and impl ProviderMethodResolver

--- src/main/java/com/huandc/Repository/mapper/StudentMapper.java
+++ src/main/java/com/huandc/Repository/mapper/StudentMapper.java
@@ -4,6 +4,7 @@ import com.huandc.model.Student;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.builder.annotation.ProviderMethodResolver;
 import org.apache.ibatis.jdbc.SQL;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
@@ -13,17 +14,17 @@ import java.util.Optional;
 
 @Mapper
 public interface StudentMapper {
-    @SelectProvider(method = "findAll", type = StudentMapper.SelectProviderSql.class)
+    @SelectProvider(type = StudentMapper.SelectProviderSql.class)
     List<Student> findAll();
 
-    @SelectProvider(method = "findById", type = StudentMapper.SelectProviderSql.class)
+    @SelectProvider(type = StudentMapper.SelectProviderSql.class)
     Optional<Student> findById(@Param("id") String id);
 
-    @SelectProvider(method = "findByIds", type = StudentMapper.SelectProviderSql.class)
+    @SelectProvider(type = StudentMapper.SelectProviderSql.class)
     List<Student> findByIds(@Param("ids") List<String> ids);
 
 
-    class SelectProviderSql {
+    class SelectProviderSql implements ProviderMethodResolver {
         public static String findAll() {
             SQL sql = new SQL();
             sql.SELECT("*");
